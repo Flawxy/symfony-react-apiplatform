fb09d41283d84f10d2fea84aa3f281f245bc1110
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-09-21 11:15:30 +0200

    Setting up authentication

--- assets/js/components/Navbar.jsx
+++ assets/js/components/Navbar.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { NavLink } from 'react-router-dom'
+import authAPI from '../services/authAPI'
+import AuthContext from '../contexts/AuthContext'
 
-const Navbar = () => {
+const Navbar = ({ history }) => {
+  const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext)
+  const handleLogout = () => {
+    authAPI.logout()
+    setIsAuthenticated(false)
+    history.push('/login')
+  }
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a className="navbar-brand" href="#">SymReact</a>
+        <NavLink className="navbar-brand" to="/">SymReact</NavLink>
           <button
             className="navbar-toggler"
             type="button"
@@ -20,39 +30,29 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarColor01">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a className="nav-link" href="#">Clients</a>
+              <NavLink className="nav-link" to="/customers">Clients</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Factures</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-                href="#"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >Dropdown</a>
-              <div className="dropdown-menu">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <a className="dropdown-item" href="#">Something else here</a>
-                <div className="dropdown-divider" />
-                <a className="dropdown-item" href="#">Separated link</a>
-              </div>
+              <NavLink className="nav-link" to="/invoices">Factures</NavLink>
             </li>
           </ul>
           <ul className='navbar-nav ml-auto'>
-            <li className='nav-item'>
-              <a href='#' className='nav-link'>Inscription</a>
-            </li>
-            <li className='nav-item'>
-              <a href='#' className='btn btn-success'>Connexion !</a>
-            </li>
-            <li className='nav-item'>
-              <a href='#' className='btn btn-danger'>Déconnexion</a>
-            </li>
+            {(!isAuthenticated && (
+              <>
+                <li className='nav-item'>
+                  <NavLink to="/register" className='nav-link'>Inscription</NavLink>
+                </li>
+                <li className='nav-item'>
+                  <NavLink to="/login" className='btn btn-success'>Connexion !</NavLink>
+                </li>
+              </>
+            )) || (
+              <li className='nav-item'>
+                <button onClick={handleLogout} className='btn btn-danger'>
+                  Déconnexion
+                </button>
+              </li>
+            )}
           </ul>
         </div>
       </nav>
